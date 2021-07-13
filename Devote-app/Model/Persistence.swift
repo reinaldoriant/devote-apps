7e60f1f4943cb926d4f31f1dae1faee21b44dc26
Reinaldo <reinaldo.riant@kompas.com>
2021-07-13 14:27:20 +0700

    ! modify preview for storage

--- Devote-app/Model/Persistence.swift
+++ Devote-app/Model/Persistence.swift
@@ -31,9 +31,12 @@ struct PersistenceController {
     static var preview: PersistenceController = {
       let result = PersistenceController(inMemory: true) // switch to memory
       let viewContext = result.container.viewContext
-      for _ in 0..<10 {
+      for i in 0..<5 {
         let newItem = Item(context: viewContext) // sample data
         newItem.timestamp = Date()
+        newItem.task = "Sample task No \(i)"
+        newItem.completion = false
+        newItem.id = UUID()
       }
       do {
         try viewContext.save() //save data to memory
