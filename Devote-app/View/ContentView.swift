ea7684301fb5a81006154a4cd318d023b62fa11d
Reinaldo <reinaldo.riant@kompas.com>
2021-07-13 15:01:26 +0700

    ! modify ui

--- Devote-app/View/ContentView.swift
+++ Devote-app/View/ContentView.swift
@@ -60,50 +60,62 @@ struct ContentView: View {
     // MARK : - Body
     var body: some View {
         NavigationView {
-            VStack {
-                VStack(spacing: 16){
-                    TextField("New Task", text: $task)
+            ZStack {
+                VStack {
+                    VStack(spacing: 16){
+                        TextField("New Task", text: $task)
+                            .padding()
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(10)
+                        Button(action: {
+                            addItem()
+                        }, label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        })
+                        .disabled(isButtonDisabled)
                         .padding()
-                        .background(Color(UIColor.systemGray6))
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .background(isButtonDisabled ? Color.gray : Color.pink)
                         .cornerRadius(10)
-                    Button(action: {
-                        addItem()
-                    }, label: {
-                        Spacer()
-                        Text("SAVE")
-                        Spacer()
-                    })
-                    .disabled(isButtonDisabled)
+                    }//: VStack
                     .padding()
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .background(isButtonDisabled ? Color.gray : Color.pink)
-                    .cornerRadius(10)
+                    List {
+                        ForEach(items) { item in
+                            VStack (alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            }//: List ITem
+                        }
+                        .onDelete(perform: deleteItems)
+                    }//: List
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
                 }//: VStack
-                .padding()
-                List {
-                    ForEach(items) { item in
-                        VStack (alignment: .leading) {
-                            Text(item.task ?? "")
-                                .font(.headline)
-                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        }//: List ITem
-                    }
-                    .onDelete(perform: deleteItems)
-                }//: List
-                .navigationBarTitle("Daily Task",displayMode: .large)
-                .toolbar {
-                    #if os(iOS)
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        EditButton()
-                    }
-                    #endif
-                }//: Toolbar
-            }//: VStack
+            }//: ZSTACK
+            .onAppear(){
+                UITableView.appearance().backgroundColor = UIColor.clear
+            }
+            .navigationBarTitle("Daily Task",displayMode: .large)
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                #endif
+            }//: Toolbar
+            .background(BackgroundImageView())
+            .background(backgroundGradient.ignoresSafeArea(.all))
         }//: Navigation
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
