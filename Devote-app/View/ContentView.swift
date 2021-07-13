eb2d026cc38ce397a8038fc6a973fc174ec1286b
Reinaldo <reinaldo.riant@kompas.com>
2021-07-13 14:03:21 +0700

    ! refactor code

--- Devote-app/View/ContentView.swift
+++ Devote-app/View/ContentView.swift
@@ -9,36 +9,17 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    //MARK : - Property
+    
+    // Fetching Data
+    @Environment(\.managedObjectContext) private var viewContext// Inject data
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
+        animation: .default)// fetch request (1. entity, 2. sort descriptopr, 3. ascennding, 4. animation)
     private var items: FetchedResults<Item>
     
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                }
-                .onDelete(perform: deleteItems)
-            }//: List
-            .toolbar {
-                #if os(iOS)
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                #endif
-                
-                ToolbarItem(placement:.navigationBarTrailing) {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            } //: Toolbar
-        }//: Navigation
-    }
+    // MARK : - Function
     
     private func addItem() {
         withAnimation {
@@ -63,22 +44,40 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
+     // MARK : - Body
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                }
+                .onDelete(perform: deleteItems)
+            }//: List
+            .toolbar {
+                #if os(iOS)
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                #endif
+                
+                ToolbarItem(placement:.navigationBarTrailing) {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            } //: Toolbar
+        }//: Navigation
+    }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
+
+// MARK : - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
