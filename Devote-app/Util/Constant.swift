98670713021923ba3d8e3224d42e36d4ec2191a5
Reinaldo <reinaldo.riant@kompas.com>
2021-07-13 14:03:06 +0700

    + add constant

---
+++ Devote-app/Util/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Devote-app
+//
+//  Created by TI Digital on 13/07/21.
+//
+
+import Foundation
+
+// MARK : - Formatter
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK : - UI
+
+
+// MARK : - UX
