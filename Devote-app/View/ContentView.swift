63769a3015386eceb5569e343a2582b6218b19c7
Reinaldo <reinaldo.riant@kompas.com>
2021-07-14 08:03:37 +0700

    ! refactor code

--- Devote-app/View/ContentView.swift
+++ Devote-app/View/ContentView.swift
@@ -64,6 +64,7 @@ struct ContentView: View {
                             // TOGGLE APPEARANCE
                             isDarkMode.toggle()
                             playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         }, label: {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -81,6 +82,7 @@ struct ContentView: View {
                     Button(action: {
                         showNewTaskItem = true
                         playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
                     }, label: {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
